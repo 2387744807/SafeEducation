18b53672a1367eab578af74e67f260771e52fc9b
2387744807 <2387744807@qq.com>
2019-08-20 19:39:08 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SafeEducation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.BtnComplete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(14, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入链接：";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtUrl.Location = new System.Drawing.Point(111, 27);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(577, 23);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // BtnComplete
+            // 
+            this.BtnComplete.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnComplete.Location = new System.Drawing.Point(596, 67);
+            this.BtnComplete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BtnComplete.Name = "BtnComplete";
+            this.BtnComplete.Size = new System.Drawing.Size(91, 42);
+            this.BtnComplete.TabIndex = 2;
+            this.BtnComplete.Text = "完成课程";
+            this.BtnComplete.UseVisualStyleBackColor = true;
+            this.BtnComplete.Click += new System.EventHandler(this.BtnComplete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 127);
+            this.Controls.Add(this.BtnComplete);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "快速完成课程";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button BtnComplete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafeEducation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnComplete_Click(object sender, EventArgs e)
+        {
+            string courseId = GetId(txtUrl.Text);//获取id
+            if (courseId == "")
+            {
+                MessageBox.Show("链接错误！");
+                return;
+            }
+            //MessageBox.Show(courseId);
+            //执行访问并返回结果
+            bool result = false;
+            try
+            {
+                result = WebRequestByGet("https://weiban.mycourse.cn/pharos/usercourse/finish.do?callback=jQuery&userCourseId=" + courseId + "&tenantCode=35110001&type=1");
+            }catch(Exception ex)
+            {
+                MessageBox.Show("发生错误：\n" + ex.Message);
+            }
+            
+            if (result)
+            {
+                MessageBox.Show("成功完成！");
+            }
+            else
+            {
+                MessageBox.Show("完成失败！");
+            }
+        }
+
+        //正则方法
+        private string GetBetween(string text,string before,string after)
+        {
+            try
+            {
+                Regex rg = new Regex("(?<=(" + before + "))[.\\s\\S]*?(?=(" + after + "))", RegexOptions.Multiline | RegexOptions.Singleline);
+                return rg.Match(text).Value;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        //采用正则方式获取id
+        private string GetId(string urlText)
+        {
+            return GetBetween(urlText, "userCourseId=", "&tenantCode");
+        }
+
+        //Get访问Url
+        private bool WebRequestByGet(string url)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream s = response.GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.GetEncoding("utf-8"));
+                string result = sr.ReadToEnd();
+
+                //释放资源
+                sr.Dispose();
+                sr.Close();
+                s.Dispose();
+                s.Close();
+                //获取msg
+                if (AnalysisJson(GetBetween(result, "jQuery\\(", "\\)")) == "ok")
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        //json解析
+        private string AnalysisJson(string json)
+        {
+            try
+            {
+                JObject jobj = (JObject)JsonConvert.DeserializeObject(json);
+                return jobj["msg"].ToString();
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+    }
+}
